1b412207e269d2a617260cec4f6d20f1f78c78ae
MilovanMilovanov <ninapredninazad@gmail.com>
2024-08-26 19:29:14 +0300

    refactored popup

--- src/components/molecules/Popup/Popup.tsx
+++ src/components/molecules/Popup/Popup.tsx
@@ -30,31 +30,30 @@ function Popup({
   return (
     <>
       {isOpen ? (
-    <section className={styles.container}>
-        <div
-          className={`${styles.modal}`}
-          role={role}
-          aria-labelledby="popup-title"
-          aria-modal="true"
-        >
-          <div className={`${styles.content} ${className}`}>
-          <h3 id="popup-title" className={styles.title}>
-            {message}
-          </h3>
-          <div className={styles.btnContainer}>
-            <Button onClick={handleConfirmation} aria-label="Confirm Deletion">
-              Delete
-            </Button>
-            <Button onClick={handleCancel} aria-label="Cancel Deletion">
-              Cancel
-            </Button>
+        <section className={styles.container}>
+          <div
+            role={role}
+            aria-labelledby="popup-title"
+            aria-modal="true"
+            className={`${styles.modal} ${className}`}>
+            <div className={styles.content}>
+              <h3 id="popup-title" className={styles.title}>
+                {message}
+              </h3>
+              <div className={styles.btnContainer}>
+                <Button onClick={handleConfirmation} aria-label="Confirm Deletion">
+                  Delete
+                </Button>
+                <Button onClick={handleCancel} aria-label="Cancel Deletion">
+                  Cancel
+                </Button>
+              </div>
+            </div>
           </div>
-          </div>
-        </div>
-      <div>{children}</div>
-    </section>
-      ): 
-      <div>{children}</div>
+          {children}
+        </section>
+      ) :
+        children
       }
     </>
   );
