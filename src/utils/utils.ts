dabfa429cea9b89de7fa6b64a5721cf00f265769
MilovanMilovanov <ninapredninazad@gmail.com>
2024-09-01 13:22:35 +0300

    created mergeRefs utility

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,18 @@
-export const areObjectValuesDifferent = <T extends Record<string, any>>(obj1: T, obj2: T): boolean => (
-    Object.keys(obj1).some(key => obj1[key] !== obj2[key])
-  );
-  
+import { Ref, RefCallback, MutableRefObject } from "react";
+
+export const areObjectValuesDifferent = <T extends Record<string, any>>(
+  obj1: T,
+  obj2: T
+): boolean => Object.keys(obj1).some((key) => obj1[key] !== obj2[key]);
+
+export function mergeRefs<T>(...refs: Ref<T>[]): RefCallback<T> {
+  return (instance: T | null) => {
+    refs.forEach((ref) => {
+      if (typeof ref === "function") {
+        ref(instance);
+      } else if (ref != null) {
+        (ref as MutableRefObject<T | null>).current = instance;
+      }
+    });
+  };
+}
