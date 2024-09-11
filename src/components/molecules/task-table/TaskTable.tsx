7d34c99cec2b80b847bd84586ed5bcac4b5def02
MilovanMilovanov <ninapredninazad@gmail.com>
2024-09-11 18:26:14 +0300

    refactored TaskTable component

--- src/components/molecules/task-table/TaskTable.tsx
+++ src/components/molecules/task-table/TaskTable.tsx
@@ -1,18 +1,51 @@
-import { TableHTMLAttributes } from "react";
-import { TaskModel } from "../../../features/user-tasks/tasksSlice";
+import { TableHTMLAttributes, useCallback } from "react";
+import {
+  TaskModel,
+  updateTaskStatus,
+} from "../../../features/user-tasks/tasksSlice";
+import { useDispatch } from "react-redux";
 
+import useApi from "../../../hooks/useApi/useApi";
 import Button from "../../atoms/button/Button";
 import Scrollable from "../../shared/scrollable/Scrollable";
 
 import styles from "./TaskTable.module.scss";
 
 interface TaskTableModel extends TableHTMLAttributes<HTMLTableElement> {
-  tabIndex?: number;
-  paginatedTasks: TaskModel[];
-  handleTaskStatusChange: (id: number, completed: boolean) => void;
+  tasks: TaskModel[];
+  currentPage: number;
 }
+const NoFilterMatchMessage =
+  "  There is no match for this filter search. Change the filter options";
 
-function TaskTable({ paginatedTasks, handleTaskStatusChange }: TaskTableModel) {
+export const PAGE_SIZE = 10;
+
+function TaskTable({ tasks, currentPage }: TaskTableModel) {
+  const { request } = useApi();
+  const dispatch = useDispatch();
+
+  const paginatedTasks = tasks.slice(
+    (currentPage - 1) * PAGE_SIZE,
+    currentPage * PAGE_SIZE
+  );
+
+  const handleTaskStatusChange = useCallback(
+    async (taskId: number, completed: boolean) => {
+      try {
+        await request({
+          method: "PATCH",
+          endpoint: `todos/${taskId}`,
+          body: JSON.stringify({ completed: !completed }),
+        });
+
+        dispatch(updateTaskStatus({ taskId, status: !completed }));
+      } catch (error) {
+        console.error("Failed to update task status:", error);
+      }
+    },
+    [request, dispatch]
+  );
+
   return (
     <table className={`base-table ${styles.table}`}>
       <thead className={styles.tableHead}>
@@ -30,34 +63,34 @@ function TaskTable({ paginatedTasks, handleTaskStatusChange }: TaskTableModel) {
         scrollWidth={0.8}
       >
         {paginatedTasks.length ? (
-          paginatedTasks.map(({ id, title, completed }: TaskModel) => (
-            <tr className={styles.row} key={id}>
-              <td className={styles.userId}>{id}</td>
-              <td className={styles.title}>{title}</td>
-              <td className={styles.status}>
-                {completed ? "Completed" : "Not Completed"}
-              </td>
-              <td className={styles.actions}>
-                <Button
-                  aria-label={
-                    completed
-                      ? "Mark task as incomplete"
-                      : "Mark task as complete"
-                  }
-                  className={styles.btnChangeTaskStatus}
-                  onClick={() => handleTaskStatusChange(id, completed)}
-                >
-                  {completed ? "Mark as Incomplete" : "Mark as Complete"}
-                </Button>
-              </td>
-            </tr>
-          ))
+          paginatedTasks.map(({ id, title, completed }: TaskModel) => {
+            const buttonText = completed
+              ? "Mark as Incomplete"
+              : "Mark as Complete";
+            return (
+              <tr className={styles.row} key={id}>
+                <td className={styles.userId}>{id}</td>
+                <td className={styles.title}>{title}</td>
+                <td className={styles.status}>
+                  {completed ? "Completed" : "Not Completed"}
+                </td>
+                <td className={styles.actions}>
+                  <Button
+                    aria-label={buttonText}
+                    className={styles.btnChangeTaskStatus}
+                    onClick={() => handleTaskStatusChange(id, completed)}
+                  >
+                    {buttonText}
+                  </Button>
+                </td>
+              </tr>
+            );
+          })
         ) : (
           <tr className={styles.noFilterMatchContainer}>
             <td>
               <span className={styles.noFilterMatchMessage}>
-                There is no match for this filter search. Change the filter
-                options
+                {NoFilterMatchMessage}
               </span>
             </td>
           </tr>
