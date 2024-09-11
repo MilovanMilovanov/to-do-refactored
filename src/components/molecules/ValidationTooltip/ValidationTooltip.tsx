62ace83cccd9484e2b92f15d950982aecd8cf240
MilovanMilovanov <ninapredninazad@gmail.com>
2024-09-11 11:18:00 +0300

    css adjustments

--- src/components/molecules/ValidationTooltip/ValidationTooltip.tsx
+++ src/components/molecules/ValidationTooltip/ValidationTooltip.tsx
@@ -7,37 +7,40 @@ import ErrorIcon from "../../../assets/icons/error-icon.svg";
 import styles from "./ValidationTooltip.module.scss";
 
 interface ValidationTooltipModel extends HTMLAttributes<HTMLElement> {
-    portalTarget: HTMLDivElement | null;
-    error: FieldError | undefined;
-};
+  portalTarget: HTMLDivElement | null;
+  error: FieldError | undefined;
+}
 
 function ValidationTooltip({
-    id,
-    error,
-    children,
-    portalTarget,
+  id,
+  error,
+  children,
+  portalTarget,
 }: ValidationTooltipModel) {
-    return (
-        <>
-            {children}
-            {error &&
-                createPortal(
-                    <>
-                        <span
-                            id={id}
-                            role="tooltip"
-                            aria-live="assertive"
-                            className={styles.tooltipError}>{error.message}</span>
-                        <ErrorIcon
-                            role="img"
-                            aria-label="error icon"
-                            className={styles.errorIcon}
-                        />
-                    </>,
-                    portalTarget as Element
-                )}
-        </>
-    );
+  return (
+    <>
+      {children}
+      {error &&
+        createPortal(
+          <>
+            <span
+              id={id}
+              role="tooltip"
+              aria-live="assertive"
+              className={styles.tooltipError}
+            >
+              {error.message}
+            </span>
+            <ErrorIcon
+              role="img"
+              aria-label="error icon"
+              className={styles.errorIcon}
+            />
+          </>,
+          portalTarget as Element
+        )}
+    </>
+  );
 }
 
 export default ValidationTooltip;
