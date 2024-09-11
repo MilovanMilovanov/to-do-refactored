4a933a38dca4ccb751afc86378f014357185fac1
MilovanMilovanov <ninapredninazad@gmail.com>
2024-09-11 18:24:54 +0300

    refactored TaskList component

--- src/components/organisms/task-list/TaskList.tsx
+++ src/components/organisms/task-list/TaskList.tsx
@@ -1,95 +1,57 @@
-import { useState, useEffect, useCallback } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { useDispatch, connect } from "react-redux";
 import { RootState } from "../../../store";
 
-import {
-  setTasks,
-  TaskModel,
-  updateTaskStatus,
-} from "../../../features/user-tasks/tasksSlice";
+import { setTasks, TaskModel } from "../../../features/user-tasks/tasksSlice";
+import { selectFilteredTasks } from "../../../features/user-tasks/tasksSelectors";
 
 import useApi from "../../../hooks/useApi/useApi";
-import TaskTable from "../../molecules/task-table/TaskTable";
+import TaskTable, { PAGE_SIZE } from "../../molecules/task-table/TaskTable";
 import TaskFilters from "../../molecules/filter-tasks/TaskFilters";
 import Pagination from "../../molecules/pagination/Pagination";
 
 import styles from "./TaskList.module.scss";
 
-const PAGE_SIZE = 10;
+interface TaskListModel {
+  tasks: TaskModel[];
+}
 
-const statusFilterOptions: string[] = ["completed", "uncompleted", "all"];
+const getFilteredTasks = (state: RootState) => ({
+  tasks: selectFilteredTasks(state),
+});
 
-function TaskList() {
+function TaskList({ tasks }: TaskListModel) {
   const { request } = useApi();
   const dispatch = useDispatch();
-
-  const tasks = useSelector((state: RootState) => state.userTasks);
-  const usersData = useSelector((state: RootState) => state.userManagement);
-  const userIds = usersData.map((user) => String(user.id));
-
-  const [filteredTasks, setFilteredTasks] = useState<TaskModel[]>([]);
   const [currentPage, setCurrentPage] = useState<number>(1);
 
   useEffect(() => {
-    async function fetchTasks() {
-      try {
-        const data = await request({ endpoint: "todos" });
-        dispatch(setTasks(data));
-        setFilteredTasks(data);
-      } catch (error) {
-        console.error("Failed to fetch tasks:", error);
+    if (tasks.length === 0) {
+      async function fetchTasks() {
+        try {
+          const data = await request({ endpoint: "todos" });
+          dispatch(setTasks(data));
+        } catch (error) {
+          console.error("Failed to fetch tasks:", error);
+        }
       }
+      fetchTasks();
     }
-    if (tasks.length === 0) fetchTasks();
-    else setFilteredTasks(tasks);
-  }, [request, dispatch, tasks]);
-
-  const handleTaskStatusChange = useCallback(
-    async (taskId: number, completed: boolean) => {
-      try {
-        await request({
-          method: "PATCH",
-          endpoint: `todos/${taskId}`,
-          body: JSON.stringify({ completed: !completed }),
-        });
-
-        dispatch(updateTaskStatus({ taskId, status: !completed }));
-      } catch (error) {
-        console.error("Failed to update task status:", error);
-      }
-    },
-    [request, dispatch]
-  );
-
-  useEffect(() => {
-    setCurrentPage(1);
-  }, [filteredTasks]);
-
-  const paginatedTasks = filteredTasks.slice(
-    (currentPage - 1) * PAGE_SIZE,
-    currentPage * PAGE_SIZE
-  );
+  }, [dispatch, request, tasks]);
 
   return (
     <section className={styles.taskList}>
-      <TaskFilters
-        tasks={tasks}
-        userIds={userIds}
-        statusFilterOptions={statusFilterOptions}
-        setFilteredTasks={setFilteredTasks}
-      />
-      <TaskTable
-        paginatedTasks={paginatedTasks}
-        handleTaskStatusChange={handleTaskStatusChange}
-      />
+      <TaskFilters />
+      <TaskTable tasks={tasks} currentPage={currentPage} />
       <Pagination
         pageSize={PAGE_SIZE}
         currentPage={currentPage}
         setCurrentPage={setCurrentPage}
-        tasks={filteredTasks}
+        tasks={tasks}
       />
     </section>
   );
 }
 
-export default TaskList;
+const TaskListConected = connect(getFilteredTasks)(TaskList);
+export default TaskListConected;
